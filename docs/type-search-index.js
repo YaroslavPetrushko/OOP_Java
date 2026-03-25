167d7f819bf204496bd5674409b1b81ae79d6549
Yaroslav Petrushko <yaroslavpetrushko.off@gmail.com>
2026-03-25 11:23:13 +0200

    Add JavaDocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"ua.edu.sumdu","l":"Book"},{"p":"ua.edu.sumdu","l":"InvalidBookDataException","k":"13"},{"p":"ua.edu.sumdu","l":"Main"}];updateSearchResults();
