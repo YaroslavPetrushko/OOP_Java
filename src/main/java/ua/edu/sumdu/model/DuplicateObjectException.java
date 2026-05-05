8e6795761d2bbeea69a5f9c0d379db16a0618867
Yaroslav Petrushko <yaroslavpetrushko.off@gmail.com>
2026-05-05 20:29:46 +0300

    Added new exceptions

---
+++ src/main/java/ua/edu/sumdu/model/DuplicateObjectException.java
@@ -0,0 +1,8 @@
+package ua.edu.sumdu.model;
+
+public class DuplicateObjectException extends RuntimeException {
+
+    public DuplicateObjectException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ua/edu/sumdu/model/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package ua.edu.sumdu.model;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
